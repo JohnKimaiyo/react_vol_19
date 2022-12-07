2c921944331ce945bc67e920bf1eaf69220243b9
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-07 11:54:23 +0300

    title prop called

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.css';
-import Greet from './component/Greet';
-import Welcome from './component/Welcome';
+import React from "react";
+import "./App.css";
+import Greet from "./component/Greet";
+import Welcome from "./component/Welcome";
+import Hello from "./component/Hello";
+import Title from "./component/Title";
 function App() {
   return (
     <div className="App">
       <Greet />
-      <Welcome/>
+      <Welcome />
+      <Hello />
+      <Title name="FrontEnd Developer" />
+      <Title name="solidity Developer" />
+      <Title name="Mobile Devloper" />
     </div>
   );
 }
---
+++ src/component/Hello.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hello = () => {
+  return React.createElement("h1", null, "Hello React Devloper Kimaiyo");
+};
+
+export default Hello;
