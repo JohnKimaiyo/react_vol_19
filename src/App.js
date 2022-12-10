30fef7bd867ccdb1dd6f7d2e4bef2139b8c46c05
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-10 12:56:38 +0300

    change name usestate

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Greet from "./component/Greet";
 import Welcome from "./component/Welcome";
 import Hello from "./component/Hello";
 import Title from "./component/Title";
+import Myname from "./component/Myname";
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
       <Title name="FrontEnd Developer" />
       <Title name="solidity Developer" />
       <Title name="Mobile Devloper" />
+      <Myname/>
     </div>
   );
 }
---
+++ src/component/Myname.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useState } from "react";
+
+export default function Myname() {
+  const [name, setName] = useState("john");
+  const changeName = () => {
+    setName("Kimaiyo");
+  };
+
+  return (
+    <div>
+      <p>my name is {name}</p>
+      <button onClick={changeName}>CLick Me</button>
+    </div>
+  );
+}
