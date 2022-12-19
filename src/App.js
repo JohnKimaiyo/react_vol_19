77fc664f23bc2dc8563eda312d0370f57c1a70ac
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-19 15:23:10 -0800

    toggle theme context api

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Navbar from "./component/Navbar";
 import BookList from "./component/BookList";
 import ThemeContextProvider from "./context/ThemeContext";
+import ThemeToggle from "./component/ThemeToggle";
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
         <Navbar />
         <BookList />
+        <ThemeToggle/>
       </ThemeContextProvider>
     </div>
   );
